6801737e3f752fa302ac20567c12aaa4aafd3566
John Lynch <lynchjohn98@gmail.com>
2025-03-01 14:08:51 -0500

    All Updates

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,16 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+// export default config;
+
+module.exports = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {}, // ✅ Ensures Tailwind works correctly
   },
 };
 
-export default config;
