e49a373226f86872177b78b8f92935767a27f066
John Lynch <lynchjohn98@gmail.com>
2025-03-26 00:08:53 -0400

    Updated package lock for json

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
+/** @type {import('postcss').Config} */
 const config = {
   plugins: {
-    "tailwindcss": {},
-    "autoprefixer": {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
 export default config;
