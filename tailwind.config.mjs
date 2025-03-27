d2a2861682f7e81994a7a77d5990116c4267c1a7
John Lynch <lynchjohn98@gmail.com>
2025-03-26 22:31:47 -0400

    Finalized updates for netlify

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,7 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+
   ],
   theme: {
     extend: {
@@ -29,3 +30,6 @@ export default {
   },
   plugins: [],
 };
+
+export default config;
+
