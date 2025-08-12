fc7279cfbbd76a35ca784eefd38c797102ca3afb
John Lynch <lynchjohn98@gmail.com>
2025-08-12 10:08:09 -0400

    Removed module pages to fix netlify error

--- app/student/student-join/page.jsx
+++ app/student/student-join/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { studentCourseJoin } from "../actions";
+import { studentCourseJoin } from "../../services/actions";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
