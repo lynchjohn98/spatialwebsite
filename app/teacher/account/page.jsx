e68fd9594e4557fb2b770cb3b664e5896cdb140c
John Lynch <lynchjohn98@gmail.com>
2025-08-12 10:46:37 -0400

    Final updates with quizzes

--- app/teacher/account/page.jsx
+++ app/teacher/account/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { retrieveTeacherCourse } from "../../services/teacher_actions";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function TeacherAccountPage() {
