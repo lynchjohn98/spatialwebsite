71773d2f94d9c7b5095a27688e40822efb4ea561
John Lynch <lynchjohn98@gmail.com>
2025-08-25 23:04:08 -0400

    Student progress fixed

--- app/teacher/progress/page.jsx
+++ app/teacher/progress/page.jsx
@@ -1,12 +1,6 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
-import StudentResponsiveQuiz from "../../../../../components/student_components/StudentResponsiveQuiz";
-import { quizData } from "../../../../library/quiz_data/combining_solids_quiz.js";
-import { submitStudentQuiz } from "../../../../library/services/student_services/student_quiz"
-
-export default function StudentModuleQuiz() {
+export default function ProgressPage() {
 
     return (
         <h1>
